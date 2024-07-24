e03b0b0b8644baabc4287b52a5a9118e0cf10b1b
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-24 18:06:26 -0300

    clean code

--- businessLogic/UserLogic.js
+++ businessLogic/UserLogic.js
@@ -1,4 +1,5 @@
 const LightItClientErrors = require('../lightItErrors/LightItClientErrors');
+
 class UserLogic {
     RegisterUser(userDTO) {
         try {
@@ -13,34 +14,34 @@ function validateUser(userDTO) {
     valideUserName(userDTO.name);
     validateEmail(userDTO.emailAddress);
     validatePhoneNumber(userDTO.phoneNumber);
-    if(!userDTO.image)
+    if (!userDTO.image)
         throw new LightItClientErrors.BadRequestError('Image is required');
-    
+
 }
 
 const ValidateUserRegex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
 function valideUserName(name) {
-    if (!name) 
+    if (!name)
         throw new LightItClientErrors.BadRequestError('Name is required');
-    if (name.length < 3) 
+    if (name.length < 3)
         throw new LightItClientErrors.BadRequestError('Name is too short, minium 3 characters');
-    if (!ValidateUserRegex.test(name)) 
+    if (!ValidateUserRegex.test(name))
         throw new LightItClientErrors.BadRequestError('Name is invalid, no special characters allowed');
 }
 
 const validateEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 function validateEmail(email) {
-    if (!email) 
+    if (!email)
         throw new LightItClientErrors.BadRequestError('Email is required');
-    if (!validateEmailRegex.test(email)) 
+    if (!validateEmailRegex.test(email))
         throw new LightItClientErrors.BadRequestError('Email is invalid');
 }
 
 const validatePhoneNumberRegex = /^\d{9}$/;
 function validatePhoneNumber(phoneNumber) {
-    if (!phoneNumber) 
+    if (!phoneNumber)
         throw new LightItClientErrors.BadRequestError('Phone number is required');
-    if (!validatePhoneNumberRegex.test(email)) 
+    if (!validatePhoneNumberRegex.test(email))
         throw new LightItClientErrors.BadRequestError('Phone number is invalid, must be 9 digits, no spaces or special characters');
 }
 
