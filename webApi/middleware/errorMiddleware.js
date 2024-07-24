4bc632a362e1d0cf66a6d5a9fd28073b2bf31a05
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-24 20:57:43 -0300

    catch new error on errorMiddleware

--- webApi/middleware/errorMiddleware.js
+++ webApi/middleware/errorMiddleware.js
@@ -1,7 +1,7 @@
 const LightItClientErrors = require('../../lightItErrors/LightItClientErrors');
  
 const errorMiddleware = (err, req, res, next) => {
-    if(err instanceof LightItClientErrors.BadRequestError) {
+    if(err instanceof LightItClientErrors.BadRequestError || err instanceof LightItClientErrors.ForbiddenError){
         return res.status(err.statusCode).send({ message: err.message});
     }
 
