3484d7b8b4a0fa2d0a52b1a3b5ae1d36f91e8338
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-24 18:03:25 -0300

    Add validations

--- businessLogic/UserLogic.js
+++ businessLogic/UserLogic.js
@@ -11,6 +11,11 @@ class UserLogic {
 
 function validateUser(userDTO) {
     valideUserName(userDTO.name);
+    validateEmail(userDTO.emailAddress);
+    validatePhoneNumber(userDTO.phoneNumber);
+    if(!userDTO.image)
+        throw new LightItClientErrors.BadRequestError('Image is required');
+    
 }
 
 const ValidateUserRegex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
@@ -21,6 +26,22 @@ function valideUserName(name) {
         throw new LightItClientErrors.BadRequestError('Name is too short, minium 3 characters');
     if (!ValidateUserRegex.test(name)) 
         throw new LightItClientErrors.BadRequestError('Name is invalid, no special characters allowed');
+}
+
+const validateEmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+function validateEmail(email) {
+    if (!email) 
+        throw new LightItClientErrors.BadRequestError('Email is required');
+    if (!validateEmailRegex.test(email)) 
+        throw new LightItClientErrors.BadRequestError('Email is invalid');
+}
+
+const validatePhoneNumberRegex = /^\d{9}$/;
+function validatePhoneNumber(phoneNumber) {
+    if (!phoneNumber) 
+        throw new LightItClientErrors.BadRequestError('Phone number is required');
+    if (!validatePhoneNumberRegex.test(email)) 
+        throw new LightItClientErrors.BadRequestError('Phone number is invalid, must be 9 digits, no spaces or special characters');
 }
 
 module.exports = new UserLogic();
