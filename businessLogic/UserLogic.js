1a3f5886f9ecfcbc47863135b738e109ae12340d
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-24 17:10:41 -0300

    Use custom errors

--- businessLogic/UserLogic.js
+++ businessLogic/UserLogic.js
@@ -1,8 +1,8 @@
+const LightItClientErrors = require('../lightItErrors/LightItClientErrors');
 class UserLogic {
     RegisterUser(userDTO) {
         try {
             validateUser(userDTO);
-            
         } catch (error) {
             console.log(error);
         }
@@ -15,9 +15,12 @@ function validateUser(userDTO) {
 
 const ValidateUserRegex = /^[A-Za-z]+(?: [A-Za-z]+)*$/;
 function valideUserName(name) {
-    if (!name) throw new Error('Name is required');
-    if (name.length < 3) throw new Error('Name is too short, minium 3 characters');
-    if (!ValidateUserRegex.test(name)) throw new Error('Name is invalid, no special characters allowed');
+    if (!name) 
+        throw new LightItClientErrors.BadRequestError('Name is required');
+    if (name.length < 3) 
+        throw new LightItClientErrors.BadRequestError('Name is too short, minium 3 characters');
+    if (!ValidateUserRegex.test(name)) 
+        throw new LightItClientErrors.BadRequestError('Name is invalid, no special characters allowed');
 }
 
 module.exports = new UserLogic();
