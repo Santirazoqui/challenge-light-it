657d8bced8919a27a2a21178f549c3d7b0bb42b0
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-24 23:16:01 -0300

    Add response status code
    return 201 on successful create

--- webApi/userController/UserController.js
+++ webApi/userController/UserController.js
@@ -5,7 +5,7 @@ async function  PostUser (req, res) {
     const { name, emailAddress, phoneNumber, image } = req.body;
     const userDTO = new UserModels.UserDTO(name, emailAddress, phoneNumber, image);
     const userCreated = await userLogic.RegisterUser(userDTO);
-    res.send(userCreated);
+    res.status(201).send(userCreated);
 }
 
 module.exports = { PostUser };
