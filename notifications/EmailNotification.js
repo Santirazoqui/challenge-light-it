15cc16f09a79fe16347edbb42a212eec4af493e7
Santirazoqui <71331155+Santirazoqui@users.noreply.github.com>
2024-07-25 00:18:22 -0300

    Send email notification

--- notifications/EmailNotification.js
+++ notifications/EmailNotification.js
@@ -1,16 +1,51 @@
 const redis = require('redis');
+const nodemailer = require('nodemailer');
 
-try {
-    const redisClient = redis.createClient().on("error", (error) => { throw new Error(error) });
-    const emailNotificationClient = redisClient.duplicate();
-    await emailNotificationClient.connect();
-    emailNotificationClient.subscribe("User_Created", async (message) => {
-        const user = JSON.parse(message);
-        console.log(`Sending email to ${user.emailAddress}`);
-        
-        console.log(`Email sent to ${user.emailAddress}`);
-        console.log("\n");
-    });
-} catch (error) {
-    console.log(error)
+async function EmailNotification(){
+    try {
+        const redisClient = redis.createClient().on("error", (error) => { throw new Error(error) });
+        const emailNotificationClient = redisClient.duplicate();
+        await emailNotificationClient.connect();
+        emailNotificationClient.subscribe("User_Created", async (message) => {
+            const user = JSON.parse(message);
+            console.log(`Sending email to ${user.emailAddress}`);
+    
+            SendMail(user);
+            
+            console.log("\n");
+        });
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+function SendMail(user){
+    const transporter = nodemailer.createTransport({
+        host: 'sandbox.smtp.mailtrap.io',
+        port: 587,
+        secure: false,
+        auth: {
+            user: '72ac2ad1e5e12e',
+            pass: 'a78d3399912c3e',
+        }
+    });
+
+    const mailOptions = {
+        from: 'irazoqui.santi@outlook.com',
+        to: `${user.emailAddress}`,
+        subject: 'User registration confirmed',
+        text: `User ${user.name} has been registered successfully!`
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+        console.log('Error:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+    });
+}
+
+(async () => {
+    EmailNotification();
+})();
